a5f7e6f2d3eabec32834199b3c2a451b19eff0b9
Pelt10 <pelt10@live.fr>
2018-10-18 21:25:29 +0200

    Update to spigot 1.12.2-R0.1

--- src/main/java/net/zyuiop/fastsurvival/lootreplacer/LootReplacer.java
+++ src/main/java/net/zyuiop/fastsurvival/lootreplacer/LootReplacer.java
@@ -24,12 +24,14 @@
 
 package net.zyuiop.fastsurvival.lootreplacer;
 
+import net.minecraft.server.v1_12_R1.NBTTagByte;
+import net.minecraft.server.v1_12_R1.NBTTagCompound;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
-import org.bukkit.block.ContainerBlock;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -39,10 +41,8 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.material.MaterialData;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
@@ -51,83 +51,85 @@ import java.util.Random;
  */
 public class LootReplacer implements Listener {
 
-	public LootReplacer(ConfigurationSection configuration) {
-		for (Map<?,?> map : configuration.getMapList("rules")) {
-			LootRule rule = LootRule.addRule(map);
-			if (rule != null) {
-				Bukkit.getLogger().info("[LootReplacer] Replaced " + rule.getSource() + " loots with " + rule.getTarget().getItemType() + ", multiplying stack size by " + rule.getMultiplier());
-			}
-		}
-	}
+    public LootReplacer(ConfigurationSection configuration) {
+        for (Map<?, ?> map : configuration.getMapList("rules")) {
+            LootRule rule = LootRule.addRule(map);
+            if (rule != null) {
+                Bukkit.getLogger().info("[LootReplacer] Replaced " + rule.getSource() + " loots with " + rule.getTarget().getItemType() + ", multiplying stack size by " + rule.getMultiplier());
+            }
+        }
+    }
 
-	public static boolean isMarked(ItemStack stack) {
-		return stack.getItemMeta() != null && stack.getItemMeta().getDisplayName() != null && stack.getItemMeta().getDisplayName().startsWith("§r");
-	}
+    public static boolean isMarked(ItemStack stack) {
+        net.minecraft.server.v1_12_R1.ItemStack craftStack = CraftItemStack.asNMSCopy(stack);
+        return craftStack.getTag() != null && craftStack.getTag().getByte("fastsurvival") == (byte)1;
+    }
 
-	public static ItemStack mark(ItemStack stack) {
-		if (isMarked(stack))
-			return stack;
+    public static ItemStack mark(ItemStack stack) {
+        if (isMarked(stack))
+            return stack;
 
-		ItemMeta meta = stack.getItemMeta();
-		meta.setDisplayName("§r" + meta.getDisplayName());
-		ItemStack ret = stack.clone();
-		ret.setItemMeta(meta);
-		return ret;
-	}
 
-	@EventHandler
-	public void playerDrop(PlayerDropItemEvent event) {
-		event.getItemDrop().setItemStack(mark(event.getItemDrop().getItemStack())); // Avoid duplication bug.
-	}
+        net.minecraft.server.v1_12_R1.ItemStack craftStack = CraftItemStack.asNMSCopy(stack);
+        NBTTagCompound nbtTag = craftStack.getTag() != null ? craftStack.getTag() : new NBTTagCompound();
+        nbtTag.set("fastsurvial", new NBTTagByte((byte)1));
 
-	@EventHandler
-	public void blockBreak(BlockBreakEvent event) {
-		BlockState state = event.getBlock().getState();
-		if (state instanceof InventoryHolder) {
-			Inventory inventory = ((InventoryHolder) state).getInventory();
-			for (int slot = 0; slot < inventory.getSize(); slot++) {
-				ItemStack stack = inventory.getItem(slot);
-				if (stack != null) {
-					inventory.setItem(slot, mark(stack));
-				}
-			}
-		}
-	}
+        net.minecraft.server.v1_12_R1.ItemStack ret = craftStack.cloneItemStack();
+        ret.setTag(nbtTag);
+        return CraftItemStack.asBukkitCopy(ret);
+    }
 
-	@EventHandler
-	public void blockExplode(EntityExplodeEvent entityExplodeEvent) {
-		for (Block block : entityExplodeEvent.blockList()) {
-			BlockState state = block.getState();
-			if (state instanceof InventoryHolder) {
-				Inventory inventory = ((InventoryHolder) state).getInventory();
-				for (int slot = 0; slot < inventory.getSize(); slot++) {
-					ItemStack stack = inventory.getItem(slot);
-					if (stack != null) {
-						inventory.setItem(slot, mark(stack));
-					}
-				}
-			}
-		}
-	}
+    @EventHandler
+    public void playerDrop(PlayerDropItemEvent event) {
+        event.getItemDrop().setItemStack(mark(event.getItemDrop().getItemStack())); // Avoid duplication bug.
+    }
 
-	@EventHandler
-	public void itemSpawn(ItemSpawnEvent event) {
-		ItemStack stack = event.getEntity().getItemStack();
-		if (stack == null || isMarked(stack))
-			return;
+    @EventHandler
+    public void blockBreak(BlockBreakEvent event) {
+        BlockState state = event.getBlock().getState();
+        if (state instanceof InventoryHolder) {
+            Inventory inventory = ((InventoryHolder) state).getInventory();
+            for (int slot = 0; slot < inventory.getSize(); slot++) {
+                ItemStack stack = inventory.getItem(slot);
+                if (stack != null) {
+                    inventory.setItem(slot, mark(stack));
+                }
+            }
+        }
+    }
 
-		Material material = stack.getType();
-		LootRule rule = LootRule.getRule(material);
-		if (rule == null)
-			return;
+    @EventHandler
+    public void blockExplode(EntityExplodeEvent entityExplodeEvent) {
+        for (Block block : entityExplodeEvent.blockList()) {
+            BlockState state = block.getState();
+            if (state instanceof InventoryHolder) {
+                Inventory inventory = ((InventoryHolder) state).getInventory();
+                for (int slot = 0; slot < inventory.getSize(); slot++) {
+                    ItemStack stack = inventory.getItem(slot);
+                    if (stack != null) {
+                        inventory.setItem(slot, mark(stack));
+                    }
+                }
+            }
+        }
+    }
 
-		if (rule.getProbability() < 1 && new Random().nextDouble() > rule.getProbability())
-			return;
+    @EventHandler
+    public void itemSpawn(ItemSpawnEvent event) {
+        ItemStack stack = event.getEntity().getItemStack();
+        if (stack == null || isMarked(stack))
+            return;
 
-		MaterialData target = rule.getTarget();
-		ItemStack replace = new ItemStack(target.getItemType(), stack.getAmount() * rule.getMultiplier());
-		replace.setData(target);
+        Material material = stack.getType();
+        LootRule rule = LootRule.getRule(material);
+        if (rule == null)
+            return;
+        if (rule.getProbability() < 1 && new Random().nextDouble() > rule.getProbability())
+            return;
+        MaterialData target = rule.getTarget();
+        ItemStack replace = new ItemStack(target.getItemType(), stack.getAmount() * rule.getMultiplier());
+        replace.setData(target);
 
-		event.getEntity().setItemStack(mark(replace));
-	}
+        event.getEntity().setItemStack(mark(replace));
+    }
 }
